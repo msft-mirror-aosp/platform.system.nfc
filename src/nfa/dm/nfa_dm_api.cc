master-hax/aosp-nfc

Extending setControllerAlwaysOn feature with transparent and Card Emulation mode support
Bug: 331563246
Test: Manual
nfcOemExtension.setControllerAlwaysOn(INIT_MODE_TRANSPARENT)
nfcOemExtension.setControllerAlwaysOn(INIT_MODE_EE)

Change-Id: I9358e849cc3437a409e939099f90cffdc09d998d

src/nfa/dm/nfa_dm_api.cc
@@ -30,6 +30,7 @@
 #include "nfa_api.h"
 #include "nfa_ce_int.h"
 #include "nfa_wlc_int.h"
+#include "nfc_int.h"
 
 using android::base::StringPrintf;
 
@@ -40,6 +41,60 @@ using android::base::StringPrintf;
 /*****************************************************************************
 **  APIs
 *****************************************************************************/
+/*******************************************************************************
+**
+** Function         NFA_SetNfccMode
+**
+** Description      This function sets the different NFC controller modes.
+**
+**                  mode INIT_MODE_DEFAULT or INIT_MODE_TRANSPARENT
+**                  or INIT_MODE_EE
+**
+** Returns          none
+**
+*******************************************************************************/
+extern void NFA_SetNfccMode(uint8_t mode) {
+  LOG(DEBUG) << StringPrintf("%s: (%d) -> (%d)", __func__, nfc_cb.nfcc_mode,
+                             mode);
+  nfc_cb.nfcc_mode = mode;
+}
+
+/*******************************************************************************
+**
+** Function         NFA_Partial_Init
+**
+** Description      This function initializes control blocks for NFA based on
+**                  mode
+**
+**                  p_hal_entry_tbl points to a table of HAL entry points
+**                  mode INIT_MODE_DEFAULT or INIT_MODE_TRANSPARENT
+**                  or INIT_MODE_EE
+**
+**                  NOTE: the buffer that p_hal_entry_tbl points must be
+**                  persistent until NFA is disabled.
+**
+** Returns          none
+**
+*******************************************************************************/
+extern void NFA_Partial_Init(tHAL_NFC_ENTRY* p_hal_entry_tbl, uint8_t mode) {
+  LOG(DEBUG) << StringPrintf("%s:enter ", __func__);
+  if (mode == INIT_MODE_TRANSPARENT) {
+    nfa_sys_init();
+    nfa_dm_init();
+  } else if (mode == INIT_MODE_EE) {
+    nfa_sys_init();
+    nfa_dm_init();
+    nfa_ee_init();
+  } else {
+    LOG(DEBUG) << StringPrintf("Unknown Mode!");
+    return;
+  }
+  /* Initialize NFC module */
+  NFC_Init(p_hal_entry_tbl);
+  NFA_SetNfccMode(mode);
+  LOG(DEBUG) << StringPrintf("%s:exit ", __func__);
+}
+
 /*******************************************************************************
 **
 ** Function         NFA_Init

src/nfc/nfc/nfc_main.cc
@@ -32,6 +32,7 @@
 #include "ce_int.h"
 #include "gki.h"
 #include "nci_hmsgs.h"
+#include "nfa_sys.h"
 #include "nfc_int.h"
 #include "nfc_target.h"
 #include "rw_int.h"
@@ -436,7 +437,10 @@ void nfc_main_handle_hal_evt(tNFC_HAL_EVT_MSG* p_msg) {
       break;
 
     case HAL_NFC_POST_INIT_CPLT_EVT:
-      if (nfc_cb.p_nci_init_rsp) {
+      if (nfc_cb.nfcc_mode == INIT_MODE_TRANSPARENT) {
+        nfc_set_state(NFC_STATE_IDLE);
+        nfa_sys_cback_notify_partial_enable_complete(NFA_ID_SYS);
+      } else if (nfc_cb.p_nci_init_rsp) {
         /*
         ** if NFC_Disable() is called before receiving
         ** HAL_NFC_POST_INIT_CPLT_EVT, then wait for HAL_NFC_CLOSE_CPLT_EVT.

msft-mirror-aosp/platform.system.nfc

src/nfa/dm/nfa_dm_api.cc
@@ -30,6 +30,7 @@
 #include "nfa_api.h"
 #include "nfa_ce_int.h"
 #include "nfa_wlc_int.h"
+#include "nfc_int.h"
 
 using android::base::StringPrintf;
 
@@ -40,6 +41,60 @@ using android::base::StringPrintf;
 /*****************************************************************************
 **  APIs
 *****************************************************************************/
+/*******************************************************************************
+**
+** Function         NFA_SetNfccMode
+**
+** Description      This function sets the different NFC controller modes.
+**
+**                  mode ENABLE_MODE_DEFAULT or ENABLE_MODE_TRANSPARENT
+**                  or ENABLE_MODE_EE
+**
+** Returns          none
+**
+*******************************************************************************/
+extern void NFA_SetNfccMode(uint8_t mode) {
+  LOG(DEBUG) << StringPrintf("%s: (%d) -> (%d)", __func__, nfc_cb.nfcc_mode,
+                             mode);
+  nfc_cb.nfcc_mode = mode;
+}
+
+/*******************************************************************************
+**
+** Function         NFA_Partial_Init
+**
+** Description      This function initializes control blocks for NFA based on
+**                  mode
+**
+**                  p_hal_entry_tbl points to a table of HAL entry points
+**                  mode ENABLE_MODE_DEFAULT or ENABLE_MODE_TRANSPARENT
+**                  or ENABLE_MODE_EE
+**
+**                  NOTE: the buffer that p_hal_entry_tbl points must be
+**                  persistent until NFA is disabled.
+**
+** Returns          none
+**
+*******************************************************************************/
+extern void NFA_Partial_Init(tHAL_NFC_ENTRY* p_hal_entry_tbl, uint8_t mode) {
+  LOG(DEBUG) << StringPrintf("%s:enter ", __func__);
+  if (mode == ENABLE_MODE_TRANSPARENT) {
+    nfa_sys_init();
+    nfa_dm_init();
+  } else if (mode == ENABLE_MODE_EE) {
+    nfa_sys_init();
+    nfa_dm_init();
+    nfa_ee_init();
+  } else {
+    LOG(ERROR) << StringPrintf("Unknown Mode!");
+    return;
+  }
+  /* Initialize NFC module */
+  NFC_Init(p_hal_entry_tbl);
+  NFA_SetNfccMode(mode);
+  LOG(DEBUG) << StringPrintf("%s:exit ", __func__);
+}
+
 /*******************************************************************************
 **
 ** Function         NFA_Init

src/nfc/nfc/nfc_main.cc
@@ -32,6 +32,7 @@
 #include "ce_int.h"
 #include "gki.h"
 #include "nci_hmsgs.h"
+#include "nfa_sys.h"
 #include "nfc_int.h"
 #include "nfc_target.h"
 #include "rw_int.h"
@@ -436,7 +437,10 @@ void nfc_main_handle_hal_evt(tNFC_HAL_EVT_MSG* p_msg) {
       break;
 
     case HAL_NFC_POST_INIT_CPLT_EVT:
-      if (nfc_cb.p_nci_init_rsp) {
+      if (nfc_cb.nfcc_mode == ENABLE_MODE_TRANSPARENT) {
+        nfc_set_state(NFC_STATE_IDLE);
+        nfa_sys_cback_notify_partial_enable_complete(NFA_ID_SYS);
+      } else if (nfc_cb.p_nci_init_rsp) {
         /*
         ** if NFC_Disable() is called before receiving
         ** HAL_NFC_POST_INIT_CPLT_EVT, then wait for HAL_NFC_CLOSE_CPLT_EVT.